f1499a1df0a84efa6b9f987f3b185dff36b4e61e
Gelx <f4lkebert@gmail.com>
2014-10-10 16:29:33 +0200

    Move packets to seperate package Add unpacking of packets Add handle() to packtehandler

--- src/io/github/gelx_/wifiaccess/net/Protocol.java
+++ src/io/github/gelx_/wifiaccess/net/Protocol.java
@@ -1,113 +1,12 @@
 package io.github.gelx_.wifiaccess.net;
 
-import io.github.gelx_.wifiaccess.database.DB_users;
-
 import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
+import io.github.gelx_.wifiaccess.net.packets.*;
 
 /**
  * Created by Falk on 08.10.2014.
  */
 public class Protocol {
-
-    public abstract class Packet{
-        public abstract short getID();
-        public abstract byte[] getData();
-    }
-
-    public final class RegisterUserPacket extends Packet{
-        private DB_users user;
-        private byte[] data;
-
-        public RegisterUserPacket(byte[] data){
-            this.data = data;
-            this.user = DB_users.fromBytes(data);
-        }
-
-        public DB_users getUser() {
-            return user;
-        }
-        public short getID(){
-            return 1;
-        }
-        public byte[] getData(){
-            return data;
-        }
-    }
-
-    public final class GetUserPacket extends Packet{
-        private String name;
-        private byte[] data;
-
-        public GetUserPacket(byte[] data){
-            this.data = data;
-            this.name = new String(data, Charset.defaultCharset());
-        }
-
-        public String getName(){
-            return name;
-        }
-        public short getID(){
-            return 2;
-        }
-        public byte[] getData(){
-            return data;
-        }
-    }
-
-    public final class GetUsers extends Packet{
-        public short getID(){
-           return 3;
-        }
-        public byte[] getData(){
-           return new byte[0];
-        }
-    }
-
-    public final class RespUser extends Packet{
-        private DB_users user;
-        private byte[] data;
-
-        public RespUser(DB_users user){
-            this.user = user;
-            this.data = user.toBytes();
-        }
-
-        public DB_users getUser(){
-            return user;
-        }
-        public short getID(){
-            return 4;
-        }
-        public byte[] getData(){
-            return data;
-        }
-    }
-
-    public final class RespUsers extends Packet{
-
-        private DB_users[] users;
-        private byte[] data;
-
-        public RespUsers(DB_users[] users){
-            this.users = users;
-            ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
-            for(DB_users user : users){
-                buffer.put(user.toBytes());
-            }
-            byte[] compacted = new byte[buffer.position()];
-            buffer.flip();
-            buffer.get(compacted);
-            this.data = compacted;
-        }
-
-        public short getID(){
-            return 5;
-        }
-        public byte[] getData(){
-            return data;
-        }
-    }
 
     public static ByteBuffer packPacket(Packet packet){
         ByteBuffer buffer = ByteBuffer.allocateDirect(6 + packet.getData().length);
@@ -116,5 +15,14 @@ public class Protocol {
         buffer.put(packet.getData());
         buffer.flip();
         return buffer;
+    }
+
+    public static Packet unpackPacket(short id, ByteBuffer data){
+        switch (id){
+            case 1: return new RegisterUserPacket(data.array());
+            case 2: return new GetUserPacket(data.array());
+            case 3: return new GetUsersPacket();
+            default: throw new IllegalArgumentException("That packetID is not meant to be received!");
+        }
     }
 }
